6665d63e72310670c1144a74b34557936fa74da8
bilibili <1763833947@qq.com>
2017-04-26 00:25:44 +0800

    实现读取ini配置文件功能

--- src/db/database.go
+++ src/db/database.go
@@ -1,3 +1,28 @@
 package db
 
+import (
+	"os"
+	"ini"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
+	"fmt"
+)
+
+var mysql *sqlx.DB
+var postgre *sqlx.DB
+
+func init() {
+
+}
+
+func ConnectMysql() {
+	var err error
+	workDir,_ := os.Getwd()
+	config := ini.SetConfig(workDir+"/src/conf/conf.ini")
+	mysql, err = sqlx.Connect("mysql", "")
+	if err != nil {
+		fmt.Printf("Can't connect to MySQL for:    %v\n", err)
+	}
+}
 
--- src/ini/conf.go
+++ src/ini/conf.go
@@ -10,7 +10,7 @@ import (
 
 type Config struct {
 	filepath string
-	conflist[]map[string]map[string]string
+	confmap map[string]map[string]string
 }
 
 func SetConfig(filepath string) (c *Config) {
@@ -21,110 +21,59 @@ func SetConfig(filepath string) (c *Config) {
 }
 
 func (c *Config) GetValue(section, name string) string {
-	data := c.conflist
-	for _,v := range data {
-		m,ok := v[section]
-		if !ok {
-			return nil
-		}
-		return m[name]
-	}
-	return nil
+	return c.confmap[section][name]
+}
+
+func (c *Config) GetSection(section string) map[string]string {
+	return c.confmap[section]
 }
 
 func (c *Config) SetValue(section, key, value string) bool {
-	data := c.conflist
 
-	var ok bool
-	var conf = make(map[string]map[string]string)
-
-	for i,v := range data {
-		_,ok = v[section]
-		if ok {
-			c.conflist[i][section][key] = value
-			break
-		}
-	}
-	if !ok {
-		conf[section] = make(map[string]string)
-		conf[section][key] = value
-		c.conflist = append(c.conflist, conf)
-	}
-
-	/*for i,v := range data {
-		_,ok = v[section]
-		index[i] = ok
-	}
-	i, ok := func(m map[int]bool) (i int, v bool) {
-		for i, v = range m {
-			if v == true {
-				return i, true
-			}
-		}
-		return 0, false
-	}(index)
-
-	if ok {
-		c.conflist[i][section][key] = value
+	if _,ok := c.confmap[section];ok {
+		c.confmap[section][key] = value
 		return true
 	} else {
-		conf[section] = make(map[string]string)
-		conf[section][key] = value
-		c.conflist = append(c.conflist, conf)
-		return true
-	}*/
-
-	return true
+		c.confmap[section] = make(map[string]string)
+		c.confmap[section][key] = value
+		return false
+	}
 }
 
-func (c *Config) readList()[]map[string]map[string]string {
+func (c *Config) readList() map[string]map[string]string {
 	file, err := os.Open(c.filepath)
 	if err != nil {
 		fmt.Sprintf("Error -> %s", err.Error())
 		return nil
 	}
 	defer file.Close()
-	var data map[string]map[string]string
+	c.confmap = make(map[string]map[string]string)
 	var section string
 	buf := bufio.NewReader(file)
 	for  {
-		l, err := buf.ReadString('\n')
-		line := strings.TrimSpace(l)
+		strLine, err := buf.ReadString('\n')
+		line := strings.TrimSpace(strLine)
 		if err != nil {
 			if err != io.EOF {
 				fmt.Sprintf("Error -> %s", err.Error())
 				return nil
 			}
-			if len(line) == 0 {
-				break
-			}
+			break
 		}
 		switch  {
 		case len(line) == 0:
 		case line[0] == '#':
 		case line[0] == '[' && line[len(line)-1] == ']':
 			section = strings.TrimSpace(line[1:len(line)-1])
-			data = make(map[string]map[string]string)
-			data[section] = make(map[string]string)
+			if _,ok := c.confmap[section]; !ok {
+				c.confmap[section] = make(map[string]string)
+			}
+			fmt.Sprintf("Error -> A same section named %s has exist!", section)
 		case strings.Contains(line, "="):
 			i := strings.IndexAny(line, "=")
-			value := strings.TrimSpace(line[i+1:len(line)-1])
-			data[section][strings.TrimSpace(line[0:i])] = value
-			if c.uniqueappend(section) == true {
-				c.conflist = append(c.conflist, data)
-			}
+			value := strings.TrimSpace(line[i+1:])
+			c.confmap[section][strings.TrimSpace(line[0:i])] = value
 		}
 	}
-	return c.conflist
-}
-
-func (c *Config) uniqueappend(conf string) bool {
-	for _,v:=range c.conflist {
-		for k,_:=range v {
-			if k==conf {
-				return false
-			}
-		}
-	}
-	return true
+	return c.confmap
 }
